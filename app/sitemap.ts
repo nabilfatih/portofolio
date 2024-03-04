0f83e6f1d54290942a094eae58fb63b13deefa31
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-03-04 11:13:25 +0100

    updates

---
+++ app/sitemap.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next"
+
+const pathnames = ["", "/", "/work"]
+
+export default function sitemap() {
+  const baseUrl = "https://www.nabilfatih.com"
+  const paths: MetadataRoute.Sitemap = pathnames.map(pathname => {
+    return {
+      url: `${baseUrl}${pathname}`,
+      lastModified: new Date().toISOString().split("T")[0],
+      changeFrequency: "weekly",
+      priority: 1
+    }
+  })
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date().toISOString().split("T")[0],
+      changeFrequency: "daily",
+      priority: 1
+    },
+    ...paths
+  ]
+}

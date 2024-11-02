7eca14b8e3467b29314a75a460f728e6a8b5d2a5
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-11-02 09:59:44 +0100

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -41,8 +41,7 @@ export default function Home() {
                 Nakafa
               </Link>
             </Button>
-            , where I build AI personalized assistant designed specifically for
-            students and teachers. I love maine coon, travel, and be with my{" "}
+            . I love maine coon, travel, and be with my{" "}
             <Button asChild variant="link" className="h-auto p-0 text-base">
               <Link href="https://instagram.com/nisrinahn_" target="_blank">
                 lovely person
